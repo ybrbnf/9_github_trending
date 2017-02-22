aa3c8160ac1ce90e58c2a58c9318b47b8380234d
Nikita Tkachenko <ybrbnf418@gmail.com>
2017-02-22 09:02:03 +0400

    исправлена функция get_repositories_info, глобальные переменные вынесены в шапку

--- github_trending.py
+++ github_trending.py
@@ -3,43 +3,42 @@ import requests
 import json
 
 
-def get_time_period():
+API_URL = 'https://api.github.com/search/repositories'
+TOP_SIZE = 20
+
+
+def get_last_week_date():
     today = datetime.date.today()
     week = datetime.timedelta(days=7)
-    time_period = today - week
-    return time_period
+    last_week_date = today - week
+    return last_week_date
 
 
-def get_trending_repositories(top_size, time_period):
-    api_url = 'https://api.github.com/search/repositories'
-    params = {'q': 'created:>={0}'.format(time_period), 'sort': 'stars'}
+def get_trending_repositories(top_size, last_week_date, api_url):
+    params = {'q': 'created:>={0}'.format(last_week_date), 'sort': 'stars'}
     response = requests.get(api_url, params=params)
     repositories = json.loads(response.text)
     return repositories['items'][:top_size]
 
 
 def get_repositories_info(repositories):
-    repositories_info = []
-    for repository in repositories:
-        repositories_info.append(
-                                 {
-                                  'url': repository['html_url'],
-                                  'stars': repository['stargazers_count'],
-                                  'owner': repository['owner']['login'],
-                                  'open_issues': repository['open_issues']
-                                 }
-                                )
+    pattern = ['html_url', 'stargazers_count', 'open_issues']
+    repositories_info = [
+                         {x: repo[x] for x in repo if x in pattern}
+                         for repo in repositories
+                         ]
     return repositories_info
 
 
 def output_repositories_info_to_console(repositories_info):
-    for item in repositories_info:
-        print(item)
+    for repo in repositories_info:
+        print('URL: {0}'.format(repo.get('html_url')))
+        print('\t''Stars: {0}'.format(repo.get('stargazers_count')))
+        print('\t''Open issues: {0}'.format(repo.get('open_issues')))
 
 
 if __name__ == '__main__':
-    time_period = get_time_period()
-    top_size = 20
-    repositories = get_trending_repositories(top_size, time_period)
+    last_week_date = get_last_week_date()
+    repositories = get_trending_repositories(TOP_SIZE, last_week_date, API_URL)
     repositories_info = get_repositories_info(repositories)
     output_repositories_info_to_console(repositories_info)
